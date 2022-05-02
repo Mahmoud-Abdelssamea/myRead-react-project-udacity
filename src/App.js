1f5fc2c7073caf85deba19a98e4ca4824b58c762
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-05-02 21:45:31 +0300

    initialize the routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Books from "./Pages/Books";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/create" element={<Search />} />
+        <Route exact path="/all" element={<Books />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
