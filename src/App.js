dc722aea3707bf0a18a9b51ccf4ecc7200c92f8a
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-05-02 22:27:48 +0300

    debug route mistake

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/create" element={<Search />} />
+        <Route exact path="/search" element={<Search />} />
         <Route exact path="/all" element={<Books />} />
       </Routes>
     </div>
