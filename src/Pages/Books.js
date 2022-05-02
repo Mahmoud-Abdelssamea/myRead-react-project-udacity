ca154e7da50523bd2b861d294b53a613c8944b11
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-05-02 21:44:51 +0300

    create Books.js page and Search page

---
+++ src/Pages/Books.js
@@ -0,0 +1,7 @@
+import "../App.css";
+
+function Books() {
+  return <div>Books</div>;
+}
+
+export default Books;
---
+++ src/Pages/Search.js
@@ -0,0 +1,6 @@
+import "../App.css"
+function Search() {
+  return <div>Search</div>;
+}
+
+export default Search;
