e2595f2896b31d4df3dc274b0245031003944096
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-05-03 03:53:37 +0300

    remove Books.js Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Books from "./Pages/Books";
 import Home from "./Pages/Home";
 import Search from "./Pages/Search";
 
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/search" element={<Search />} />
-        <Route exact path="/all" element={<Books />} />
       </Routes>
     </div>
   );
